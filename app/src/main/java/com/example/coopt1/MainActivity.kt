76ed96c59fa1a333b8fcf5b7000e725cc18c66e4
Kwan Yi Fong <kfong2@academic.rrc.ca>
2023-09-09 00:02:06 -0500

    update

--- app/src/main/java/com/example/coopt1/MainActivity.kt
+++ app/src/main/java/com/example/coopt1/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.coopt1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , OnInitListener {
+
+    private lateinit var textToSpeech: TextToSpeech // declares a variable of type TextToSpeech
+    private lateinit var textToSpeak: TextView
+    private lateinit var speakButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textToSpeech = TextToSpeech(this, this) // initiate TextToSpeech
+        textToSpeak = findViewById(R.id.textToSpeak) // refers to the TextView with id "textToSpeak"
+        speakButton = findViewById(R.id.speakButton)
+
+        speakButton.setOnClickListener {
+            val text = textToSpeak.text.toString() // Get text from TextView with id "textToSpeak"
+            if (text.isNotEmpty()) {
+                textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null) // QUEUE_FLUSH, stops the ongoing speech and replaces new one (can use QUEUE_ADD to add new one to line)
+            }
+        }
+    }
+
+    // override of the onInit method from the TextToSpeech.OnInitListener
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.US)
+
+            // the language data is missing OR not supported
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Text to Speech not supported on this device.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Text to Speech failed.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // overrides the onDestroy method from the Activity class
+    override fun onDestroy() {
+        if (textToSpeech.isSpeaking) {
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown() // turns off textToSpeech feature
+        super.onDestroy()
     }
 }
